6237701fc549a056ce5fb21a8d85341ad9424cd4
Gustavo Leonardo <leonardomoralesgustavo@gmail.com>
2023-11-02 12:03:52 -0600

    topbar new structure

--- components/UI/TopBar.tsx
+++ components/UI/TopBar.tsx
@@ -1,6 +1,17 @@
-import React from "react";
-import { Box, AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { useRouter } from "next/router";
+import { Badge, Icon } from "@mui/material";
+import NextLink from "next/link";
 
 interface IMenuProfile {
   title: string;
@@ -10,28 +21,220 @@ interface IMenuProfile {
 
 interface IPages {
   title: string;
+  icon?: any;
   route: string;
 }
 
+const pages: IPages[] = [
+  {
+    title: "Inicio",
+    route: "/",
+  },
+];
+
 export const TopBar = () => {
+  const router = useRouter();
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ bgcolor: "transparent"}}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Texto
-          </Typography>
+    <AppBar
+      position="static"
+      sx={{ bgcolor: "transparent", boxShadow: "none" }}
+    >
+      <Toolbar disableGutters sx={{ mx: 4 }}>
+        <Box
+          component={"img"}
+          src="./images/logo.png"
+          width={"10%"}
+          height={"60px"}
+          sx={{
+            display: { xs: "none", md: "flex" },
+            my: 2,
+            mr: 1,
+            cursor: "pointer",
+          }}
+          onClick={() => router.push("/")}
+        />
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
           <IconButton
             size="large"
-            edge="start"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
           >
             <MenuIcon />
           </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: "block", md: "none" },
+            }}
+          >
+            {pages.map(({ title, route }) => (
+              <MenuItem key={title} onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">{title}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "none", md: "flex" },
+            justifyContent: "flex-end",
+          }}
+        >
+          {pages.map(({ title, icon, route }) => (
+            <NextLink
+              key={route}
+              href={route}
+              passHref
+              style={{ textDecoration: "none" }}
+            >
+              <Button
+                key={title}
+                onClick={() => {
+                  handleCloseNavMenu(), router.push(`${route}`);
+                }}
+                sx={{
+                  my: 2,
+                  mr: 4,
+                  display: "block",
+                  color: router.pathname == route ? "black" : "white",
+                  fontWeight: router.pathname == route ? "900" : null,
+                }}
+              >
+                {title}
+                <Badge
+                  // badgeContent={title === "" ? ticketIds.length : 0}
+                  color="error"
+                >
+                  {icon}
+                </Badge>
+              </Button>
+            </NextLink>
+          ))}
+        </Box>
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Abrir menu">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              {/* <PersonOutlineIcon sx={{ color: "white" }} /> */}
+              <MenuIcon />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {/* {!isLogged ? (
+              <>
+                {noLogged.map(({ title, icon, route }) => (
+                  <NextLink
+                    key={route}
+                    href={route}
+                    passHref
+                    style={{ textDecoration: "none" }}
+                  >
+                    <MenuItem
+                      key={title}
+                      onClick={() => {
+                        handleCloseUserMenu(), router.push(`${route}`);
+                      }}
+                    >
+                      <Box sx={{ mr: 1, mt: -0.25 }}>
+                        <>{icon}</>
+                      </Box>
+                      <Typography textAlign="center">{title}</Typography>
+                    </MenuItem>
+                  </NextLink>
+                ))}
+              </>
+            ) : (
+              <>
+                {settings.map(({ title, icon, route }) => (
+                  <NextLink
+                    key={route}
+                    href={route}
+                    passHref
+                    style={{ textDecoration: "none" }}
+                  >
+                    <MenuItem
+                      key={title}
+                      onClick={() => {
+                        handleCloseUserMenu(), router.push(`${route}`);
+                      }}
+                      sx={{
+                        bgcolor: router.pathname == route ? "#CFA53A" : null,
+                        color: router.pathname == route ? "white" : "black",
+                        "&:hover": {
+                          bgcolor: "#EDC663",
+                          color: "black",
+                        },
+                      }}
+                    >
+                      <Box
+                        sx={{
+                          mr: 1,
+                          mt: -0.25,
+                        }}
+                      >
+                        <>{icon}</>
+                      </Box>
+                      <Typography textAlign="center">{title}</Typography>
+                    </MenuItem>
+                  </NextLink>
+                ))}
+              </>
+            )} */}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
---
+++ pages/register.tsx
@@ -0,0 +1,10 @@
+import CreateUserForm from '@/components/sections/user/forms/CreateUserForm/CreateUserForm'
+import React from 'react'
+
+const register = () => {
+  return (
+    <CreateUserForm/>
+  )
+}
+
+export default register
